6f81da3eff321696883d05cd7ed39a1036d809ec
gideonn12 <155386445+gideonn12@users.noreply.github.com>
2024-09-13 17:38:44 +0300

    starting tracking

--- Object_detection.py
+++ Object_detection.py
@@ -11,4 +11,20 @@ class ObjectDetection():
         net = cv2.dnn.readNet(weight_path, cfg_path)
 
         net.setPreferableBackend(cv2.dnn.DNN_BACKEND_DEFAULT)	
+        net.setPreferableTarget(cv2.dnn.DNN_BACKEND_DEFAULT)
+        self.model = cv2.dnn_DetectionModel(net)
         
+        self.classes = []
+        self.load_class_names()
+        self.colors = np.random.uniform(0, 255, size=(80, 3))	
+
+        self.model.setInputParams(size=(self.image_size, self.image_size), scale=1/255)
+
+    def load_class_names(self, class_path=r'C:\Users\gideo\Desktop\yolo4\dnn_model\classes.txt'):
+        with open(class_path, 'r') as f:
+            self.classes = f.read().splitlines()
+        self.color = np.random.uniform(0, 255, size=(80, 3))
+        return self.classes
+        
+    def detect(self, frame):
+        return self.model.detect(frame, self.nms_threshold, self.cong_threshold)
